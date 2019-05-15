2db32a3bd2ce18cfa165c18f54e31c047a940b4c
铁柱， <jym@jiyoumi.com>
2019-05-15 14:27:53 +0800

    海尔筑家申请 页面完善

--- pages/haierHouse/HaierHouseApply.vue
+++ pages/haierHouse/HaierHouseApply.vue
@@ -6,8 +6,9 @@
         <li class="bt2-houseApply-card-item">
           <text class="bt2-houseApply-card-item-name">筑家负责人</text>
         </li>
-        <li class="bt2-houseApply-card-item">
+        <li class="bt2-houseApply-card-item uni-column">
           <text class="bt2-houseApply-card-item-name">手机号码</text>
+					<input class="uni-input" placeholder-style="color:#999999;line-height:56upx" type="number" maxlength="11" @blur="telEnd()" v-model="tel" placeholder="请输入"/>
         </li>
       </ul>
     </view>
@@ -33,6 +34,27 @@
             </label>
           </radio-group>
         </li>
+				 <li class="bt2-houseApply-card-item uni-column">
+          <text class="bt2-houseApply-card-item-name">样板间面积</text>
+					<input class="uni-input" placeholder-style="color:#999999;line-height:56upx"  placeholder="请输入"/>
+					<p class="bt2-houseApply-card-item-unit">平米</p>
+				</li>
+				<li class="bt2-houseApply-card-item uni-column">
+          <text class="bt2-houseApply-card-item-name">详细地址</text>
+					<input class="uni-input" placeholder-style="color:#999999;line-height:56upx"  placeholder="请输入"/>
+
+        </li>
+				<li class="bt2-houseApply-card-item uni-column">
+          <text class="bt2-houseApply-card-item-name">样板间租金</text>
+					<input class="uni-input" placeholder-style="color:#999999;line-height:56upx"  placeholder="请输入"/>
+					<p class="bt2-houseApply-card-item-unit">元/月</p>
+			 </li>
+				<li class="bt2-houseApply-card-item uni-column">
+          <text class="bt2-houseApply-card-item-name">租赁日期</text>
+					<input class="uni-input-time" placeholder-style="color:#999999;line-height:56upx"  placeholder="开始时间"/>
+					<p>至</p>
+					<input class="uni-input-time" placeholder-style="color:#999999;line-height:56upx"  placeholder="结束时间"/>
+				</li>
         <li class="bt2-houseApply-card-item">
           <text class="bt2-houseApply-card-item-name">入驻产业</text>
         </li>
@@ -58,23 +80,27 @@
           <li class="bt2-houseApply-card-item">
             样板间照片
           </li>
-          <ss-upload-image :url="url" :file-list="fileList" :name="imgName" @on-success="onSuccess" @on-error=""
-                           @on-remove="onRemove"/>
+          <ss-upload-image :url="url" :file-list="fileList" :name="imgName" @on-success="onSuccess" @on-error="onError" @on-remove="onRemove"/>
         </ul>
       </view>
     </view>
+		<view class="bt2-houseApply-btn">
+			<p class="bt2-houseApply-btn-p" @click="nextPage()">下一步</p>
+		</view>
   </view>
 </template>
 
 <script>
   import ssUploadImage from '@/components/ss-upload-image/ss-upload-image.vue';
   import UniIcon from '@/components/uni-icon/uni-icon.vue';
+	import calendar from "@/components/uni-calendar/uni-calendar"
 
   export default {
     name: "HaierHouseApply",
     components: {
       ssUploadImage,
-      UniIcon
+      UniIcon,
+			calendar
     },
     data() {
       return {
@@ -82,10 +108,11 @@
         url: '',
         fileList: [],
         current: 1,
+				tel:'',
         items1: [
           {
             value: 'USA',
-            name: '毛配方'
+            name: '毛坯房'
           },
           {
             value: 'CHN',
@@ -115,13 +142,35 @@
       onSuccess() {
 
       },
+			onError(){
+				
+			},
       onRemove() {
 
-      }
+      },
+			telEnd(){
+				
+			},
+			nextPage(){
+				uni.navigateTo({
+          url: '/pages/haierHouse/HaierHouseApplySecondPage'
+        });
+			}
+			
     }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+	.uni-input{
+		// background-color: aquamarine;
+		margin-left: 50upx !important;
+		font-size: 28upx;
+	}
+	.uni-input-time{
+		margin-left: 50upx !important;
+		font-size: 28upx;
+		width: 160upx
+	}
+ 
 </style>
